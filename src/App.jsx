c9db71641340f5df3ce4610d8dd893d17a74783b
Siddhesh <siddheshghag150@gmail.com>
2025-11-17 14:49:38 +0530

    fix: header alignment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='min-h-screen w-screen flex justify-center flex-col items-center'>
+    <div className='min-h-screen w-screen flex justify-start flex-col items-center'>
       <Header/>
       {/* make home page component and mount here */}
       {/* make getInvoled page component and mount here */}
