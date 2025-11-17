67c2ef8f3f33f783a3e5274cd51cc9b23b9a5941
Siddhesh <siddheshghag150@gmail.com>
2025-11-17 21:58:56 +0530

    refactor: fixing alignment issue

--- src/components/GetInvolved.jsx
+++ src/components/GetInvolved.jsx
@@ -1,92 +1,149 @@
 function Getinvolved() {
-    return (
-        <div>
-            <h1 className="text-4xl font-semibold italic mt-5 ml-40">Get Involved - Swastik Srijan Foundation</h1>
-            <p className="mt-3 text-zinc-500 font-semibold text-center">Join us to create imapct in education, health, woman empowerment,
-                <br></br>
-                environment and more
+  return (
+    <div className="min-h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-semibold italic mt-5">
+        Get Involved - Swastik Srijan Foundation
+      </h1>
+      <p className="mt-3 text-zinc-500 font-semibold text-center">
+        Join us to create imapct in education, health, woman empowerment,
+        <br></br>
+        environment and more
+      </p>
+      <div className="mt-7 grid grid-cols-3 gap-13">
+        <div className="bg-blue-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
+          <div className="z-11 w-[270px] h-54 bg-white m-2">
+            <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Volunteer</h1>
+            <p className="font-semibold text-zinc-400 pl-3">
+              Contribute your time and passion to make a difference
             </p>
-            <div className="mt-7 grid grid-cols-3 gap-13">
-                <div  className="bg-blue-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
-                    <div className="z-11 w-[270px] h-54 bg-white m-2">
-                        <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Volunteer</h1>
-                        <p className="font-semibold text-zinc-400 pl-3">Contribute your time and passion
-                            to make a difference
-                        </p>
-                        <span className="bg-blue-300 ml-3 mt-8 text-[14px] p-1 inline-block">Community work</span>
-                        <span className="bg-blue-300 ml-3 text-[14px] p-1 inline-block">Teaching</span>
-                        <span className="bg-blue-300 ml-3 mt-2 text-[14px] p-1 inline-block">Awareness Drives</span>
-                    </div>
-                    <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110" ><a href="#">Explore</a></button>
-                </div>
-
-                <div className="bg-purple-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
-                    <div className="z-11 w-[270px] h-54 bg-white m-2">
-                        <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Member</h1>
-                        <p className="font-semibold text-zinc-400 pl-3">Join our growing family and be
-                            part of lasting charge
-                        </p>
-                        <span className="bg-purple-300 ml-3 mt-8 text-[14px] p-1 inline-block">Membership Benefit</span>
-                        <span className="bg-purple-300 ml-3 text-[14px] p-1 inline-block">Network</span>
-                        <span className="bg-purple-300 ml-3 mt-2 text-[14px] p-1 inline-block">Impact Reports</span>
-                    </div>
-                    <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110" ><a href="#">Explore</a></button>
-                </div>
-
-                <div className="bg-red-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
-                    <div className="z-11 w-[270px] h-54 bg-white m-2">
-                        <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Donor/Supporter</h1>
-                        <p className="font-semibold text-zinc-400 pl-3">Fuel Our mission with your
-                            <br></br>
-                            generous contribution
-                        </p>
-                        <span className="bg-red-300 ml-3 mt-8 text-[14px] p-1 inline-block">One-Time</span>
-                        <span className="bg-red-300 ml-3 text-[14px] p-1 inline-block">Monthly Supports</span>
-                        <span className="bg-red-300 ml-3 mt-2 text-[14px] p-1 inline-block">Awareness Drives</span>
-                    </div>
-                    <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110" ><a href="#">Explore</a></button>
-                </div>
-
-                <div className="bg-green-200 w-[290px]  h-68 shadow-[6px_6px_0px_#000]">
-                    <div className=" z-11 w-[270px] h-54 bg-white m-2">
-                        <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Partnership</h1>
-                        <p className="font-semibold text-zinc-400 pl-3">Collaborate with us to create a
-                            social impact through CSR initiatives
-                        </p>
-                        <span className="bg-green-300 ml-3 mt-2 text-[14px] p-1 inline-block">Brand Collaboration</span>
-                        <span className="bg-green-300 ml-3 text-[14px] p-1 inline-block">Join Events</span>
-                        <span className="bg-green-300 ml-3 mt-2 text-[14px] p-1 inline-block">Awareness Drives</span>
-                    </div>
-                    <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110" ><a href="#">Explore</a></button>
-                </div>
-
-                <div className="bg-orange-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
-                    <div className="z-11 w-[270px] h-54 bg-white m-2">
-                        <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Collaboration</h1>
-                        <p className="font-semibold text-zinc-400 pl-3">Let's work together to 
-                            expand and amplify change
-                        </p>
-                        <span className="bg-orange-300 ml-3 mt-8 text-[14px] p-1 inline-block">Join Compaigns</span>
-                        <span className="bg-orange-300 ml-3 text-[14px] p-1 inline-block">Co-Initiatives</span>
-                        <span className="bg-orange-300 ml-3 mt-2 text-[14px] p-1 inline-block">Awareness Program</span>
-                    </div>
-                    <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110" ><a href="#">Explore</a></button>
-                </div>
-
-                <div className="bg-pink-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
-                    <div className=" z-11 w-[270px] h-54 bg-white m-2">
-                        <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Career/Internship</h1>
-                        <p className="font-semibold text-zinc-400 pl-3">Learn, grow, and create impact
-                            with purpose-driven work
-                        </p>
-                        <span className="bg-pink-300 ml-3 mt-8 text-[14px] p-1 inline-block">Internships</span>
-                        <span className="bg-pink-300 ml-3 text-[14px] p-1 inline-block">Career Growth</span>
-                        <span className="bg-pink-300 ml-3 mt-2 text-[14px] p-1 inline-block">Skill Building</span>
-                    </div>
-                    <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110" ><a href="#">Explore</a></button>
-                </div>
-            </div>
+            <span className="bg-blue-300 ml-3 mt-8 text-[14px] p-1 inline-block">
+              Community work
+            </span>
+            <span className="bg-blue-300 ml-3 text-[14px] p-1 inline-block">
+              Teaching
+            </span>
+            <span className="bg-blue-300 ml-3 mt-2 text-[14px] p-1 inline-block">
+              Awareness Drives
+            </span>
+          </div>
+          <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110">
+            <a href="#">Explore</a>
+          </button>
         </div>
-    )
+
+        <div className="bg-purple-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
+          <div className="z-11 w-[270px] h-54 bg-white m-2">
+            <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Member</h1>
+            <p className="font-semibold text-zinc-400 pl-3">
+              Join our growing family and be part of lasting charge
+            </p>
+            <span className="bg-purple-300 ml-3 mt-8 text-[14px] p-1 inline-block">
+              Membership Benefit
+            </span>
+            <span className="bg-purple-300 ml-3 text-[14px] p-1 inline-block">
+              Network
+            </span>
+            <span className="bg-purple-300 ml-3 mt-2 text-[14px] p-1 inline-block">
+              Impact Reports
+            </span>
+          </div>
+          <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110">
+            <a href="#">Explore</a>
+          </button>
+        </div>
+
+        <div className="bg-red-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
+          <div className="z-11 w-[270px] h-54 bg-white m-2">
+            <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">
+              Donor/Supporter
+            </h1>
+            <p className="font-semibold text-zinc-400 pl-3">
+              Fuel Our mission with your
+              <br></br>
+              generous contribution
+            </p>
+            <span className="bg-red-300 ml-3 mt-8 text-[14px] p-1 inline-block">
+              One-Time
+            </span>
+            <span className="bg-red-300 ml-3 text-[14px] p-1 inline-block">
+              Monthly Supports
+            </span>
+            <span className="bg-red-300 ml-3 mt-2 text-[14px] p-1 inline-block">
+              Awareness Drives
+            </span>
+          </div>
+          <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110">
+            <a href="#">Explore</a>
+          </button>
+        </div>
+
+        <div className="bg-green-200 w-[290px]  h-68 shadow-[6px_6px_0px_#000]">
+          <div className=" z-11 w-[270px] h-54 bg-white m-2">
+            <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Partnership</h1>
+            <p className="font-semibold text-zinc-400 pl-3">
+              Collaborate with us to create a social impact through CSR
+              initiatives
+            </p>
+            <span className="bg-green-300 ml-3 mt-2 text-[14px] p-1 inline-block">
+              Brand Collaboration
+            </span>
+            <span className="bg-green-300 ml-3 text-[14px] p-1 inline-block">
+              Join Events
+            </span>
+            <span className="bg-green-300 ml-3 mt-2 text-[14px] p-1 inline-block">
+              Awareness Drives
+            </span>
+          </div>
+          <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110">
+            <a href="#">Explore</a>
+          </button>
+        </div>
+
+        <div className="bg-orange-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
+          <div className="z-11 w-[270px] h-54 bg-white m-2">
+            <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">Collaboration</h1>
+            <p className="font-semibold text-zinc-400 pl-3">
+              Let's work together to expand and amplify change
+            </p>
+            <span className="bg-orange-300 ml-3 mt-8 text-[14px] p-1 inline-block">
+              Join Compaigns
+            </span>
+            <span className="bg-orange-300 ml-3 text-[14px] p-1 inline-block">
+              Co-Initiatives
+            </span>
+            <span className="bg-orange-300 ml-3 mt-2 text-[14px] p-1 inline-block">
+              Awareness Program
+            </span>
+          </div>
+          <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110">
+            <a href="#">Explore</a>
+          </button>
+        </div>
+
+        <div className="bg-pink-200 w-[290px] h-68 shadow-[6px_6px_0px_#000]">
+          <div className=" z-11 w-[270px] h-54 bg-white m-2">
+            <h1 className="font-bold text-3xl pt-3 pl-3 pb-3">
+              Career/Internship
+            </h1>
+            <p className="font-semibold text-zinc-400 pl-3">
+              Learn, grow, and create impact with purpose-driven work
+            </p>
+            <span className="bg-pink-300 ml-3 mt-8 text-[14px] p-1 inline-block">
+              Internships
+            </span>
+            <span className="bg-pink-300 ml-3 text-[14px] p-1 inline-block">
+              Career Growth
+            </span>
+            <span className="bg-pink-300 ml-3 mt-2 text-[14px] p-1 inline-block">
+              Skill Building
+            </span>
+          </div>
+          <button className="font-semibold ml-5 transition-transform duration-500 hover:scale-110">
+            <a href="#">Explore</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Getinvolved;
